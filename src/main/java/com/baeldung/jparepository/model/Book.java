f26fdeead725aa037fb16ca1f38ec2bdf3ec4fa9
Tomas Vasylius <sangokas36@gmail.com>
2024-04-17 19:48:42 +0300

    First Commit

---
+++ src/main/java/com/baeldung/jparepository/model/Book.java
@@ -0,0 +1,35 @@
+package com.baeldung.jparepository.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
